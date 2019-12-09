832be0974a5b53bbe7f5dbaf2fc1b7fa998e884b
vermin <vermin@techie.com>
2019-12-09 22:34:12 +0100

    wazn way :green_apple:

--- src_api/config.php
+++ src_api/config.php
@@ -1,12 +1,12 @@
 <?php
 
 $daemonAddress = getenv('daemon_address') !== false ? getenv('daemon_address') : 'localhost';
-$rpcPort = getenv('daemon_rpc_port') !== false ? (int)getenv('daemon_rpc_port') : 38081;
+$rpcPort = getenv('daemon_rpc_port') !== false ? (int)getenv('daemon_rpc_port') : 11787;
 
 function transmitRequest($endpoint){
 	global $daemonAddress;
 	global $rpcPort;
-	
+
 	$url = 'http://'.$daemonAddress.':'.$rpcPort.'/'.$endpoint;
 	$ch = curl_init( $url );
 	if ( strtolower($_SERVER['REQUEST_METHOD']) == 'post' ) {
@@ -15,15 +15,15 @@ function transmitRequest($endpoint){
 		curl_setopt($ch, CURLOPT_POSTFIELDS, file_get_contents('php://input') );
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 	}
-	
+
 	curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
 	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
 	curl_setopt( $ch, CURLOPT_ENCODING ,'');
-	
+
 	$contents = curl_exec( $ch );
 	$status = curl_getinfo( $ch );
 	curl_close( $ch );
-	
+
 	header('Content-type: application/json');
 	echo $contents;
 }
